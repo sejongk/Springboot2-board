71b632c42616fe91c45cf7368621226d0a85ddc0
sepaper <sepaper@naver.com>
2021-03-09 00:55:38 +0900

    oauth - integrate with google social login

---
+++ src/main/java/com/springboot/domain/user/User.java
@@ -0,0 +1,48 @@
+package com.springboot.domain.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING) // DB에 기록될 때 기본값인 int이 아닌 string으로 저장해 해당 필드가 어떤 것을 의미하는지 바로 식별
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
--- src/main/java/com/springboot/web/IndexController.java
+++ src/main/java/com/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.springboot.web;
 
+import com.springboot.config.auth.dto.SessionUser;
 import com.springboot.services.posts.PostsService;
 import com.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,12 +9,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.mail.Session;
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor // final이 붙은 변수들을 초기화해주는 내용을 가지는 IndexController 생성자 주입
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-
+    private final HttpSession httpSession;
     //RequiredArgsConstructor 어노테이션 사용 시 아래 생성자가 자동 주입
     /*
     public IndexController(PostsService postsService){
@@ -24,6 +28,11 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser)httpSession.getAttribute("user");
+        if(user != null) {
+            System.out.println("유저가 존재하느냐 "+ user.getEmail());
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
     @GetMapping("/posts/save")
